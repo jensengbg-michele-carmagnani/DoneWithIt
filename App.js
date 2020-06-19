7972d8872771f9e5c7ed9a39cba4fab92a50ff39
Michele <michele.carmagnani@gmail.com>
2020-06-19 15:15:59 +0200

    WellcomeScreen

--- App.js
+++ App.js
@@ -1,21 +1,103 @@
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React from "react";
+// import {
+//   StyleSheet,
+//   Text,
+//   View,
+//   TouchableHighlight,
+//   SafeAreaView,
+//   Image,
+//   Button,
+//   Alert,
+//   Platform,
+//   StatusBar,
+//   Dimensions,
+// } from "react-native";
+
+// import {
+//   useDimensions,
+//   useDeviceOrientation,
+// } from "@react-native-community/hooks";
+
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  const handlePress = () => console.log("Text pressed");
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress} >Hello world!</Text>
-    </SafeAreaView>
-  );
+  //how to get the dimention of the screen.
+  // const { landscape } = useDeviceOrientation();
+  return <WelcomeScreen/>
 }
+//     // <SafeAreaView style={styles.container}>
+//     //   <View
+//     //     style={{
+//     //       backgroundColor: "dodgerblue",
+//     //       width: 100,
+//     //       height: 100,
+//     //       // width: "100%",
+//     //       // height: landscape ? "100%" : "30%",
+//     //       // flex: 1,
+//     //     }}
+//     //   />
+//     //   <View
+//     //     style={{
+//     //       backgroundColor: "yellow",
+//     //       width: 100,
+//     //       height: 100,
+//     //       position:"absolute",
+//     //       top: 70,
+//     //       left: 70,
+//     //       // width: "100%",
+//     //       // height: landscape ? "100%" : "30%",
+//     //       // flex: 1,
+//     //     }}
+//     //   />
+//     //   <View
+//     //     style={{
+//     //       backgroundColor: "tomato",
+//     //       width: 100,
+//     //       height: 100,
+//           // alignSelf:"flex-start",
+//           // width: "100%",
+//           // height: landscape ? "100%" : "30%",
+//           // flex: 1,
+//         // }}
+//       // />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'dodgerblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//       {/* <Text>Hello world!</Text>
+//         <TouchableHighlight onPress={() => console.log("Imge tapped")}>
+//           <Image
+//             source={{ uri: "https://picsum.photos/200" }}
+//             style={{ width: 200, height: 300 }}
+//             blurRadius={0}
+//           />
+//         </TouchableHighlight>
+//         <Button
+//           title="Press me"
+//           onPress={() =>
+//             Alert.alert("My tiltel", "My messge", [
+//               { text: "yes", onPress: () => console.log("Yes") },
+//               { text: "no", onPress: () => console.log("no") },
+//             ])
+//           }
+//         />
+//         <Button
+//           title="Press me"
+//           color="#f194ff"
+//           onPress={() =>
+//             Alert.prompt("My title", "My text", (text) => console.log(text))
+//           }
+//         /> */}
+//     {/* </SafeAreaView> */}
+// //   );
+// // }
+
+// // const styles = StyleSheet.create({
+// //   container: {
+// //     flex: 1,
+// //     flexWrap: "wrap",
+// //     alignContent: "center", // it allign the entire content in main axes
+// //     alignItems: "center", //it align content only in sencond axes
+
+// //     backgroundColor: "#fff",
+// //     flexDirection: "row",
+// //     justifyContent: "center",
+// //   },
+// // });
